4be46503245411f977b9318ccc8855d3cfa2311e
Sean Ishii <Sean.Ishii@va.gov>
2025-02-13 10:46:56 -1000

    finished chapter 12

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -14,7 +14,7 @@ export default async function Page(props: {
   }>;
 }) {
   const searchParams = await props.searchParams;
-  const query = searchParams.query || "";
+  const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
 
